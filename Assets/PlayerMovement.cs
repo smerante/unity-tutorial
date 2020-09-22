03894251f3a126615fdab9959b4f7569ecb6e022
smerante <merante.s@hotmail.com>
2020-09-22 19:35:21 -0400

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    // Fixed update is called based on physics and fps
+    void FixedUpdate()
+    {
+        rigidBody.AddForce(0, 0, 500 * Time.deltaTime);
+    }
+}
