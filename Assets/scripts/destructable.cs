5766d17d4a8044bcef1de481dd474fd791ba116d
smerante <merante.s@hotmail.com>
2020-09-24 17:55:25 -0400

    destructable and score

---
+++ Assets/scripts/destructable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructable : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(this.tag + " : " + (collision.gameObject.tag));
+        if (collision.gameObject.tag == "Player" && this.tag == "Obstacle")
+        {
+            Instantiate(this.destroyedVersion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.text = "" + Math.Round(this.playerTransform.position.z);
+    }
+}
