55b692eea7cd2229302b8c2924c5039f2e1fe865
smerante <merante.s@hotmail.com>
2020-09-28 19:46:58 -0400

    feat: CharacterController movement

--- Assets/scripts/MouseMevement.cs
+++ Assets/scripts/MouseMevement.cs
@@ -12,9 +12,9 @@ public class MouseMevement : MonoBehaviour
         if (mouseDown)
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.Log("2d Screen Mouse: " + Input.mousePosition.x + " " + Input.mousePosition.y);
+            //Debug.Log("2d Screen Mouse: " + Input.mousePosition.x + " " + Input.mousePosition.y);
 
-            Debug.Log("Player: " + this.transform.position.x + " " + this.transform.position.z);
+            //Debug.Log("Player: " + this.transform.position.x + " " + this.transform.position.z);
             RaycastHit hit = new RaycastHit();
 
             if (Physics.Raycast(ray, out hit))
@@ -24,7 +24,7 @@ public class MouseMevement : MonoBehaviour
                 float c = Mathf.Sqrt(x * x + z * z);
                 float angle = Mathf.Asin((z / c));
                 float angleDeg = Mathf.Rad2Deg * angle;
-                Debug.Log("Distance: " + c + " : Angle: " + angle + "degrees: " + angleDeg + " : " + x + " : " + z);
+                //Debug.Log("Distance: " + c + " : Angle: " + angle + "degrees: " + angleDeg + " : " + x + " : " + z);
 
 
                 Vector3 forceVector = new Vector3(-x, 0f, -z) * (this.moveSpeed / 100);
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotateSpeed = 180f;
+    public float moveSpeed = 10f;
+    float gravity = -9.8f;
+    public CharacterController characterController;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.4f;
+    public LayerMask groundLayer;
+    bool isGrounded;
+    public float jumpHeight = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            this.transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            this.transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("w"))
+        {
+            Vector3 moveDirection = transform.TransformDirection(Vector3.forward) * moveSpeed * Time.deltaTime;
+            if (this.characterController)
+            {
+                this.characterController.Move(moveDirection);
+            }
+        }
+        if (Input.GetKey("s"))
+        {
+            Vector3 moveDirection = transform.TransformDirection(-Vector3.forward) * moveSpeed * Time.deltaTime;
+            if (this.characterController)
+            {
+                this.characterController.Move(moveDirection);
+            }
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity) * Time.deltaTime;
+        }
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f * Time.deltaTime;
+        }
+        velocity.y += gravity*1.5f * Time.deltaTime * Time.deltaTime;
+        characterController.Move(velocity);
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("ControllerColliderHit: " + hit.gameObject.name);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision: " + collision.gameObject.name);
+        Vector3 moveDirection = transform.TransformDirection(collision.rigidbody.velocity) * moveSpeed * Time.deltaTime;
+        this.characterController.Move(moveDirection);
+    }
+}
